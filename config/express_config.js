a7fc827a294977cd851e01c39aa2c62654d0292b
KM <KM@KMui-Macintosh.local>
2017-11-21 23:36:39 +0900

    bugfixes

--- config/express_config.js
+++ config/express_config.js
@@ -53,7 +53,7 @@ module.exports = function() {
     var programs = require('../routes/programs');
     var reservations = require('../routes/reservations');
     var coupons = require('../routes/coupons');
-    var couponPurchaseLogs = require('../routes/var reservations = require('../routes/reservations');');
+    var couponPurchaseLogs = require('../routes/couponPurchaseLogs');
 
     var admin = require('../routes/admin'); // 관리자 페이지
     var customers = require('../routes/customers'); // 사용자용 페이지 --> 테스트용
