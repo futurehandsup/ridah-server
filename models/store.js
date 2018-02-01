3b6ae6a485991e83ea4adaef6c6fe4b06d65f5c7
KM <KM@KMui-Macintosh.local>
2018-02-01 18:18:09 +0900

    스토어 위치기반 검색 쿼리 작성

--- models/store.js
+++ models/store.js
@@ -59,8 +59,13 @@ var StoreSchema = new Schema({
       default : "/images/background.jpg"
     },
     gps : {
-      type : String,
-      default: ""
+      type : {
+        type: String,
+        default: "Point"
+      },
+      coordinates : {
+        type: Array
+      }
     },
     // owner : {
     //   type : Schema.ObjectId,
@@ -153,4 +158,7 @@ StoreSchema.set('toJSON',{ getters : true , virtuals : true});
 /*res.json() 을 사용하여 다큐먼트 데이터를 출력 할 때 get 옵션으로 정의한 값이 JSON에 포함되게 할 것이다.
 위 코드를 적지 않으면 JSON으로 데이터를 표현할 때 get 옵션을 무시하게 될 것 이다.
 출처: http://alexband.tistory.com/23 [Front-end Rider] */
+
+StoreSchema.index({ gps : "2dsphere" } );
+
 mongoose.model('Store', StoreSchema);
