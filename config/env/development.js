65ad4ea818cc7e806a818e1ffaeb00dec1d764f8
dhffkvm15 <dhffkvm15@gmail.com>
2020-02-06 15:11:49 +0900

    cache remove

--- config/env/development.js
+++
@@ -1,7 +0,0 @@
-var dbinfo = require('./_db.js')
-/* 개발자용 옵션 */
-module.exports = {
-  db : 'mongodb://'+dbinfo.database_id+':'+dbinfo.database_pw+'@localhost/database',
-  sessionSecret : 'thisissessionSecret',
-  refreshSecret : 'thisisrefreshsessionSecret',
-}
