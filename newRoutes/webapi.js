9c2b9293736979698cc72c89005067ed3f2c4595
dhffkvm15 <dhffkvm15@gmail.com>
2020-02-18 14:25:54 +0900

    5. Api 작성

--- newRoutes/webapi.js
+++ newRoutes/webapi.js
@@ -5,6 +5,15 @@ var common = require('../newControllers/common');
 
 //컨트롤러 파일 추가!
 var members = require('../newControllers/members');
+var zzims = require('../newControllers/zzims');
+var reservations = require('../newControllers/reservations');
+var reviews = require('../newControllers/reviews');
+var stores = require('../newControllers/stores');
+var programs = require('../newControllers/programs');
+var notices = require('../newControllers/notices');
+var banners = require('../newControllers/banners');
+var schedules = require('../newControllers/schedules');
+var faqs = require('../newControllers/faqs');
 // var stores = require('../newControllers/stores');
 // ...
 
@@ -21,6 +30,141 @@ router.route('/member/:userNo')
 .get(members.getMemberDetail, common.setResponse)     //사용자 정보 출력
 .put(members.updateMember, common.setResponse)        //사용자 정보 가져오기
 .delete(members.deleteMember, common.setResponse)     //사용자 삭제
+.post(common.notImplementedError);
+
+// zzims
+router.route('/zzim')
+.all(function(req, res, next)  {next();})
+.get(zzims.getZzimList, common.setResponse) // 찜 리스트 출력
+.post(zzims.addZzim, common.setResponse)    // 찜 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/zzim/:zzimNo')
+.all(function(req, res, next) {next();})
+.get(zzims.getZzimDetail, common.setResponse)     //찜 정보 출력
+.put(zzims.updateZzim, common.setResponse)        //찜 정보 가져오기
+.delete(zzims.deleteZzim, common.setResponse)     //찜 삭제
+.post(common.notImplementedError);
+
+// reservations
+router.route('/reservation')
+.all(function(req, res, next)  {next();})
+.get(reservations.getReservationList, common.setResponse) //예약 리스트 출력
+.post(reservations.addReservation, common.setResponse)    //예약 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/reservation/:reservationNo')
+.all(function(req, res, next) {next();})
+.get(reservations.getReservationDetail, common.setResponse)     //예약 정보 출력
+.put(reservations.updateReservation, common.setResponse)        //예약 정보 가져오기
+.delete(reservations.deleteReservation, common.setResponse)     //예약 삭제
+.post(common.notImplementedError);
+
+// reviews
+router.route('/review')
+.all(function(req, res, next)  {next();})
+.get(reviews.getReviewList, common.setResponse) //후기 리스트 출력
+.post(reviews.addReview, common.setResponse)    //후기 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/review/:reviewNo')
+.all(function(req, res, next) {next();})
+.get(reviews.getReviewDetail, common.setResponse)     //후기 정보 출력
+.put(reviews.updateReview, common.setResponse)        //후기 정보 가져오기
+.delete(reviews.deleteReview, common.setResponse)     //후기 삭제
+.post(common.notImplementedError);
+
+// stores
+router.route('/store')
+.all(function(req, res, next)  {next();})
+.get(stores.getStoreList, common.setResponse) //가맹점 리스트 출력
+.post(stores.addStore, common.setResponse)    //가맹점 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/store/:storeNo')
+.all(function(req, res, next) {next();})
+.get(stores.getStoreDetail, common.setResponse)     //가맹점 정보 출력
+.put(stores.updateStore, common.setResponse)        //가맹점 정보 가져오기
+.delete(stores.deleteStore, common.setResponse)     //가맹점 삭제
+.post(common.notImplementedError);
+
+// programs
+router.route('/program')
+.all(function(req, res, next)  {next();})
+.get(programs.getProgramList, common.setResponse) //프로그램 리스트 출력
+.post(programs.addProgram, common.setResponse)    //프로그램 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/program/:programNo')
+.all(function(req, res, next) {next();})
+.get(programs.getProgramDetail, common.setResponse)     //프로그램 정보 출력
+.put(programs.updateProgram, common.setResponse)        //프로그램 정보 가져오기
+.delete(programs.deleteProgram, common.setResponse)     //프로그램 삭제
+.post(common.notImplementedError);
+
+// notices
+router.route('/notice')
+.all(function(req, res, next)  {next();})
+.get(notices.getNoticeList, common.setResponse) //공지사항 리스트 출력
+.post(notices.addNotice, common.setResponse)    //공지사항 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/notice/:noticeNo')
+.all(function(req, res, next) {next();})
+.get(notices.getNoticeDetail, common.setResponse)     //공지사항 정보 출력
+.put(notices.updateNotice, common.setResponse)        //공지사항 정보 가져오기
+.delete(notices.deleteNotice, common.setResponse)     //공지사항 삭제
+.post(common.notImplementedError);
+
+// banners
+router.route('/banner')
+.all(function(req, res, next)  {next();})
+.get(banners.getBannerList, common.setResponse) //배너 리스트 출력
+.post(banners.addBanner, common.setResponse)    //배너 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/banner/:bannerNo')
+.all(function(req, res, next) {next();})
+.get(banners.getBannerDetail, common.setResponse)     //배너 정보 출력
+.put(banners.updateBanner, common.setResponse)        //배너 정보 가져오기
+.delete(banners.deleteBanner, common.setResponse)     //배너 삭제
+.post(common.notImplementedError);
+
+// schedules
+router.route('/program')
+.all(function(req, res, next)  {next();})
+.get(programs.getProgramList, common.setResponse) //프로그램 리스트 출력
+.post(programs.addProgram, common.setResponse)    //프로그램 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/program/:programNo')
+.all(function(req, res, next) {next();})
+.get(programs.getProgramDetail, common.setResponse)     //프로그램 정보 출력
+.put(programs.updateProgram, common.setResponse)        //프로그램 정보 가져오기
+.delete(programs.deleteProgram, common.setResponse)     //프로그램 삭제
+.post(common.notImplementedError);
+
+// faqs
+router.route('/faq')
+.all(function(req, res, next)  {next();})
+.get(faqs.getFaqList, common.setResponse) //faq 리스트 출력
+.post(faqs.addFaq, common.setResponse)    //faq 등록
+.put(common.notImplementedError)
+.delete(common.notImplementedError);
+
+router.route('/faq/:faqNo')
+.all(function(req, res, next) {next();})
+.get(faqs.getFaqDetail, common.setResponse)     //faq 정보 출력
+.put(faqs.updateFaq, common.setResponse)        //faq 정보 가져오기
+.delete(faqs.deleteFaq, common.setResponse)     //faq 삭제
 .post(common.notImplementedError);
 
 // 원래 코드 참고
